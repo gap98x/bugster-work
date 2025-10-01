1c6d7c5af24457ccdf4378d114859183fc638d75
Gabriel <gabrielmmn98@gmail.com>
2025-10-01 08:49:00 -0300

    test

---
+++ components/banners/new-arrival-banner.tsx
@@ -0,0 +1,7 @@
+export function NewArrivalBanner() {
+  return (
+    <div className="px-4 py-2 bg-blue-600 text-white text-center text-sm font-medium">
+      Check out our latest new arrivals!
+    </div>
+  );
+}
